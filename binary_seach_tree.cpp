5b541fcab5a516db9fc10e784740cf8f357a9b3b
Connor <your_email@example.com>
2026-03-12 12:49:45 -0700

    added a new method find_parent to simplify insertion, removal, and searching. also finished the initial implementation of remove

--- binary_seach_tree.cpp
+++ binary_seach_tree.cpp
@@ -94,15 +94,15 @@ public:
 
     void insert(K key)
     {
-        Node *node_to_insert = Node(key_to_insert, nullptr, nullptr, nullptr);
+        Node *node_to_insert = Node(key, nullptr, nullptr, nullptr);
 
         // search the proper sub-branch of trav
-        Node *parent = find_parent(key_to_insert);
+        Node *parent = find_parent(key);
 
         // only update parent attributes if the insertion is not at the root
         if (parent != nullptr)
         {
-            if (is_larger_key(key_to_insert, parent->key))
+            if (is_larger_key(key, parent->key))
             {
                 parent->right_child = node_to_insert;
             }
@@ -120,23 +120,105 @@ public:
 
     void remove(K key)
     {
+        Node *to_removes_parent = find_parent(key);
+
         // key does not exist
-        if (!search(key))
+        if (to_removes_parent == nullptr)
         {
-            throw std::invalid_argument("Key does not exist.")
+            throw std::invalid_argument("Key does not exist.");
         }
 
-        Node *parent = find_parent(key);
-        Node *to_delete = nullptr;
+        Node *to_remove;       // determine the node which must be removed, given its parent
+        Node *is_parents_left; // the node to delete is the left child of the parent node
 
-        // parent exists
-        if (parent != nullptr)
+        if (to_removes_parent->left_child->key == to_remove->key)
         {
+            to_remove = to_removes_parent->left_child;
+            is_parents_left = true;
         }
-        // root is the only node
-        to_delete = root;
-        root = nullptr;
-        delete to_delete;
+        else
+        {
+            to_remove = to_removes_parent->right_child;
+            is_parents_left = false;
+        }
+
+        // case 1: node to delete has no children
+        // case 2: node to delete has a predecessor or successor
+        // case 3: node to delete has both a predecessor and successor
+
+        // to remove a node, either replace with the largest value in the left subtree (predecessor), or the smallest value in the right subtree (successor)
+
+        // step 1:
+        // in the case of a predecessor, update predecessors children to point to the node to deletes children
+        // in the case of a successor, update the successors children to point to the node to deletes children
+        // in the case of no predecessor or successor, nothing is required
+
+        // step 2:
+        // in the case of a predecessor, update the predecessors parent's right child node to nullptr
+        // in the case of a successor, update the successors parent's left child node to nullptr
+        // in the case of no predecessor or successor, nothing is required
+
+        // step 3:
+        // in the case of a predecessor, update the node to removes child pointer to point to the predecessor
+        // in the case of a successor, update the node to removes child pointer to point to the successor
+        // in the case of no predecessor or successor, update the node to removes child pointer to nullptr
+
+        // step 4:
+        // deallocate the memory that was used to store the node which we want to remove
+
+        Node *predecessor = find_predecessor(to_remove);
+        Node *predecessors_parent = predecessor->parent;
+        Node *successor = find_succesor(to_delete);
+        Node *successors_parent = successor->parent;
+
+        if (predecessor != nullptr)
+        {
+            // make predecessors children = children of node to delete
+            predecessor->left_child = to_delete->left_child;
+            predecessor->right_child = to_delete->right_child;
+            // update the parent of predecessor's child node to nullptr
+            predecessors_parent->right_child = nullptr;
+
+            if (is_parents_left) // to remove is the parents left child
+            {
+                to_removes_parent->left_child = predecessor;
+            }
+            else // to remove is the parents right child
+            {
+                // update the parent's child pointer from the node to delete to the predecessor
+                to_removes_parent->right_child = predecessor;
+            }
+        }
+        else if (successor != nullptr)
+        {
+            // make predecessors children = children of node to delete
+            successor->left_child = to_delete->left_child;
+            successor->right_child = to_delete->right_child;
+            // update the parent of predecessor's child node to nullptr
+            successors_parent->left_child = nullptr;
+
+            if (is_parents_left) // to remove is the parents left child
+            {
+                to_removes_parent->left_child = predecessor;
+            }
+            else // to remove is the parents right child
+            {
+                // update the parent's child pointer from the node to delete to the predecessor
+                to_removes_parent->right_child = predecessor;
+            }
+        }
+        else
+        {
+            if (is_parents_left)
+            {
+                to_removes_parent->left_child = nullptr;
+            }
+            else
+            {
+                to_removes_parent->right_child = nullptr;
+            }
+        }
+        delete to_remove;
     }
 
     bool search(K key)
